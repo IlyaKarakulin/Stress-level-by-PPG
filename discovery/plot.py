01458deb29a16ea07e20cc9433583fdc543ffb62
artem <art.skripnikov@gmail.com>
2025-04-20 17:59:05 +0700

    data2

--- discovery/plot.py
+++ discovery/plot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('/home/artem/Documents/2_course/Project ML/stress-level-by-PPG/discovery/testing.csv')  # Замените на ваш путь к файлу
+data = pd.read_csv('/home/artem/Documents/2_course/Project_ML/stress-level-by-PPG/discovery/testing.csv')  
 
 plt.style.use('ggplot')
 
@@ -9,15 +9,15 @@ fig, ax = plt.subplots(figsize=(10, 6))
 
 bars = ax.bar(
     data['state'], 
-    data['stress_index_4-24'],
-    color=['#1f77b4', '#ff7f0e', '#2ca02c'],  # Можно изменить цвета
+    data['stress_index_0-5'],
+    color=['#25DE43', '#C93D6C', '#25DE43', '#C93D6C', '#C93D6C'],  #
     width=0.6
 )
 
-ax.set_ylim(4, 24)
+ax.set_ylim(0, 5)
 ax.set_title('Stress Index by State', pad=20, fontsize=14)
 ax.set_xlabel('State', labelpad=10)
-ax.set_ylabel('Stress Index (4-24)', labelpad=10)
+ax.set_ylabel('Stress Index (0-5)', labelpad=10)
 
 for bar in bars:
     height = bar.get_height()
@@ -32,5 +32,5 @@ ax.yaxis.grid(True, linestyle='--', alpha=0.7)
 
 plt.tight_layout()
 
-plt.savefig("data.png")
+plt.savefig("data2.png")
 plt.show()
